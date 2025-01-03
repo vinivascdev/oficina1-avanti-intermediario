c8ccf717aae499aba854c5fc9ef19f61cb5709b1
vinivascdev <joaoviniciuspereiraa@gmail.com>
2025-01-03 01:28:26 -0300

    Correção de bugs

--- js/script.js
+++ js/script.js
@@ -21,15 +21,15 @@ function calcularIMC(event) {
 
     imc = peso / (altura ** 2);
 
-    if (imc <= 18.5) {
+    if (imc <= 18.50) {
         classificacao = "Abaixo do Peso";
-    } else if (imc >= 18.6 && imc <= 24.9) {
+    } else if (imc >= 18.51 && imc <= 24.99) {
         classificacao = "Peso Normal";
-    } else if (imc >= 25 && imc <= 29.9) {
+    } else if (imc >= 25 && imc <= 29.99) {
         classificacao = "Sobrepeso";
-    } else if (imc >= 30 && imc <= 34.9) {
+    } else if (imc >= 30 && imc <= 34.99) {
         classificacao = "Obesidade Grau 1";
-    } else if (imc >= 35 && imc <= 39.9) {
+    } else if (imc >= 35 && imc <= 39.99) {
         classificacao = "Obesidade Grau 2";
     } else {
         classificacao = "Obesidade Grau 3";
